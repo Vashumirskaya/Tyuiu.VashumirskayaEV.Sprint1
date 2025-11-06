8212113a995c915180c0a77f787872d0d2e94d51
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-11-06 20:30:58 +0500

    Task1

---
+++ Tyuiu.VashumirskayaEV.Sprint1.Task1.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint1.Task1.V24.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint1.Task1.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(0, res);
+        }
+    }
+}
