527fdf7569d584d40fa40e56081db14ec0e037c6
Елизавета Вашумирская <Tyuiu.vashumirskayaEV@outlook.com>
2025-11-08 19:59:50 +0500

    Task7

---
+++ Tyuiu.VashumirskayaEV.Sprint1.Task7.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint1.Task7.V21.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint1.Task7.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 12.182;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VashumirskayaEV.Sprint1.Task7.V21/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.VashumirskayaEV.Sprint1.Task7.V21.Lib;
+
+namespace Tyuiu.VashumirskayaEV.Sprint1.Task7.V21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Вашумирская Е. В. | СМАРТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #21                                                             *");
+            Console.WriteLine("* Выполнила: Вашумирская Елизавета Владимировна | СМАРТб-25-1             *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем.                      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ :                                                       *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ :                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.Calculate(x, y);
+            Console.WriteLine($"z = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}
